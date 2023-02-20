633edba609a06418a0be27f25dc0443c599ad6df
Yousri11 <yousri.meftah12@gmail.com>
2023-02-20 13:34:51 +0100

    forgot to change the value of the input so now its fixed

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const ex=document.querySelector("#change")
 const s1=document.querySelector(".s1")
 const s2=document.querySelector(".s2")
 const alls=document.querySelectorAll("select")
-
+const t1=document.querySelector('#t1')
+const t2=document.querySelector('#t2')
 for(let i=0;i<alls.length;i++){
     for(let j in countries){
         var s="";
@@ -33,5 +34,8 @@ ex.addEventListener('click',()=>{
     let c=s1.value
     s1.value=s2.value
     s2.value=c;
+    let t=t1.value
+    t1.value=t2.value
+    t2.value=t
     return ''
 })
